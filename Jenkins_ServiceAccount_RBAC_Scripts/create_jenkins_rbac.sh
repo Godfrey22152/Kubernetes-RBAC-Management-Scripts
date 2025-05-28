6bb6bba0b3a00161f5a4967461def0f98a3ba653
GODFREY IFEANYI <90422561+Godfrey22152@users.noreply.github.com>
2025-05-28 05:20:52 +0100

    Updated create_jenkins_rbac.sh

--- Jenkins_ServiceAccount_RBAC_Scripts/create_jenkins_rbac.sh
+++ Jenkins_ServiceAccount_RBAC_Scripts/create_jenkins_rbac.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-# Define namespace
+# Define Resources
 NAMESPACE="webapps"
+ROLE="jenkins-role"
+ROLEBINDING="jenkins-rolebinding"
+SECRET="jenkins-secret"
+SERVICEACCOUNT="jenkins"
+RBAC_CONFIG_FILE="jenkins_rbac_config.yaml"
+PROCESS_NAME="Jenkins ServiceAccount"
 
 # Check if namespace exists, if not, create it
 kubectl get namespace $NAMESPACE >/dev/null 2>&1
@@ -12,26 +18,26 @@ else
   echo "Namespace $NAMESPACE already exists."
 fi
 
-# Create the resources using the jenkins_rbac_config.yaml file
-echo "Creating Jenkins ServiceAccount, Role, RoleBinding, and Secret..."
-kubectl apply -f jenkins_rbac_config.yaml
+# Create the resources using the Jenkins_rbac_config.yaml file
+echo "Creating $PROCESS_NAME, Role, RoleBinding, and Secret..."
+kubectl apply -f $RBAC_CONFIG_FILE
 
 # Verify the creation of the ServiceAccount, Role, and RoleBinding
-kubectl get serviceaccount jenkins -n $NAMESPACE
-kubectl get role app-role -n $NAMESPACE
-kubectl get rolebinding app-rolebinding -n $NAMESPACE
+kubectl get serviceaccount $SERVICEACCOUNT -n $NAMESPACE
+kubectl get role $ROLE -n $NAMESPACE
+kubectl get rolebinding $ROLEBINDING -n $NAMESPACE
 
 # Wait for the secret to be created
-echo "Waiting for the secret to be created..."
+echo "Waiting for the $SECRET secret to be created..."
 sleep 5
 
 # Get the secret for the service account and display it
-SECRET_NAME=$(kubectl get secret -n $NAMESPACE | grep jenkins-secret | awk '{print $1}')
+SECRET_NAME=$(kubectl get secret -n $NAMESPACE | grep $SECRET | awk '{print $1}')
 
 if [ -z "$SECRET_NAME" ]; then
-  echo "Error: Could not find the secret for the ServiceAccount jenkins."
+  echo "Error: Could not find the $SECRET secret for the $PROCESS_NAME."
 else
-  echo "ServiceAccount secret created: $SECRET_NAME"
+  echo "$PROCESS_NAME secret created: $SECRET_NAME"
 
   # Retrieve the secret token
   SECRET_TOKEN=$(kubectl get secret $SECRET_NAME -n $NAMESPACE -o jsonpath='{.data.token}' | base64 --decode)
@@ -39,10 +45,9 @@ else
   if [ -z "$SECRET_TOKEN" ]; then
     echo "Error: Could not retrieve the token from the secret."
   else
-    echo "Secret token for the ServiceAccount jenkins:"
+    echo "Secret token for the $PROCESS_NAME:"
     echo "$SECRET_TOKEN"
   fi
 fi
 
-echo "Jenkins service account and RBAC resources created successfully!"
-
+echo "$PROCESS_NAME and RBAC resources created successfully!"
